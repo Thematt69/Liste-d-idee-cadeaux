0db0acd7e26764b42dcba66fa993fb295334ace8
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-11 21:15:17 +0100

    LDC-12 #finish : Create Import

--- widgets/import/index.php
+++ widgets/import/index.php
@@ -11,23 +11,23 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<link rel="icon" href="https://dev.matthieudevilliers.fr/images/SpaLogo.webp">
+<!-- <link rel="icon" href="https://family.matthieudevilliers.fr/images/SpaLogo.webp"> -->
 
 <!-- Normalize CSS -->
-<link href="https://familly.matthieudevilliers.fr/css/normalize.css" rel="stylesheet">
+<link href="https://family.matthieudevilliers.fr/css/normalize.css" rel="stylesheet">
 
 <!-- Bootstrap -->
-<link rel="stylesheet" href="https://familly.matthieudevilliers.fr/css/bootstrap.min.css">
-<script src="https://familly.matthieudevilliers.fr/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="https://family.matthieudevilliers.fr/css/bootstrap.min.css">
+<script src="https://family.matthieudevilliers.fr/js/bootstrap.bundle.min.js"></script>
 
 <!-- Bootstrap -->
-<script src="https://familly.matthieudevilliers.fr/js/jquery-3.5.1.min.js"></script>
+<script src="https://family.matthieudevilliers.fr/js/jquery-3.5.1.min.js"></script>
 
 <!-- Icon FontAwesome -->
-<script src="https://familly.matthieudevilliers.fr/js/45afd6dbfc.js"></script>
+<script src="https://family.matthieudevilliers.fr/js/45afd6dbfc.js"></script>
 
 <!-- Style CSS -->
-<!-- <link href="https://familly.matthieudevilliers.fr/css/style.css" rel="stylesheet"> -->
+<!-- <link href="https://family.matthieudevilliers.fr/css/style.css" rel="stylesheet"> -->
 
 <!--[if IE]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
