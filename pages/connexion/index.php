1e8cb443875c70a0c9a6f7676eff71980f4a0944
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-12 14:58:15 +0100

    LDC-41 #finish

--- pages/connexion/index.php
+++ pages/connexion/index.php
@@ -14,9 +14,39 @@
     <?php include('../../widgets/navbar/index.php'); ?>
 
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-
+        <div class="row  justify-content-center">
+            <div class="col-sm-12 col-md-9 col-lg-6">
+                <br>
+                <div class="card">
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-floating">
+                                        <input name="Mail" type="email" class="form-control" id="LabelMail" placeholder="name@example.com" required>
+                                        <label for="LabelMail">Adresse mail</label>
+                                    </div>
+                                    <br>
+                                    <div class="form-floating">
+                                        <input name="MDP" type="password" class="form-control" id="LabelMDP" placeholder="Mot de passe" required>
+                                        <label for="LabelMDP">Mot de passe</label>
+                                    </div>
+                                    <br>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary">Se connecter</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <br>
+                <div class="text-center">
+                    <a href="https://family.matthieudevilliers.fr/pages/inscription/">
+                        Pas de compte ? Je m'inscris
+                    </a>
+                </div>
+                <br>
             </div>
         </div>
     </div>
