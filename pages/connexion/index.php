d233969a37d4286b33655c1a970d1fbe2cad75fc
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-12 14:00:15 +0100

    LDC-41 #start : Create design

---
+++ pages/connexion/index.php
@@ -0,0 +1,26 @@
+<?php include('script.php'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Liste d'idée cadeaux - Connexion</title>
+
+    <!-- Import -->
+    <?php include('../../widgets/import/index.php'); ?>
+</head>
+
+<body>
+
+    <?php include('../../widgets/navbar/index.php'); ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
