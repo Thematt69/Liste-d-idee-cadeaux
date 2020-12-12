336c773281a46834afaf96930dda80fad95fe405
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-12 15:06:54 +0100

    LDC-51 #finish

--- pages/compte/index.php
+++ pages/compte/index.php
@@ -24,43 +24,46 @@
                         <form action="" method="post">
                             <div class="row">
                                 <div class="col-md-6">
-                                    <div class="form-group">
+                                    <div class="form-floating">
+                                        <input name="Prénom" type="text" class="form-control" id="LabelPrénom" placeholder="Prénom" required>
                                         <label for="LabelPrénom">Prénom</label>
-                                        <input name="Prénom" type="text" class="form-control" id="LabelPrénom">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
-                                    <div class="form-group">
+                                    <div class="form-floating">
+                                        <input name="Nom" type="text" class="form-control" id="LabelNom" placeholder="Nom" required>
                                         <label for="LabelNom">Nom</label>
-                                        <input name="Nom" type="text" class="form-control" id="LabelNom">
                                     </div>
+                                    <br>
                                 </div>
                                 <div class="col-md-6">
-                                    <div class="form-group">
+                                    <div class="form-floating">
+                                        <input name="MDP" type="password" class="form-control" id="LabelMDP" aria-describedby="DescriptionMDP" placeholder="Mot de passe" required>
                                         <label for="LabelMDP">Mot de passe</label>
-                                        <input name="MDP" type="password" class="form-control" id="LabelMDP" aria-describedby="DescriptionMDP">
                                         <small id="DescriptionMDP" class="form-text text-muted">Votre mot de passe est enregistré dans un format crypté.</small>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
-                                    <div class="form-group">
+                                    <div class="form-floating">
+                                        <input name="ConfirmationMDP" type="password" class="form-control" id="LabelConfirmationMDP" aria-describedby="DescriptionMDP" placeholder="Confirmer mot de passe" required>
                                         <label for="LabelConfirmationMDP">Confirmer mot de passe</label>
-                                        <input name="ConfirmationMDP" type="password" class="form-control" id="LabelConfirmationMDP" aria-describedby="DescriptionMDP">
                                     </div>
+                                    <br>
                                 </div>
                                 <div class="col-md-6">
-                                    <div class="form-group">
+                                    <div class="form-floating">
+                                        <input name="Naissance" type="date" class="form-control" id="LabelNaissance" aria-describedby="DescriptionNaissance" placeholder="Date de naissance" required>
                                         <label for="LabelNaissance">Date de naissance</label>
-                                        <input name="Naissance" type="date" class="form-control" id="LabelNaissance" aria-describedby="DescriptionNaissance">
                                         <small id="DescriptionNaissance" class="form-text text-muted">Votre date de naissance est demandée au personne qui souhaitent accéder à vos listes partagées en lien sécurisé.</small>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
-                                    <div class="form-group">
+                                    <div class="form-floating">
+                                        <input name="Mail" type="email" class="form-control" id="LabelMail" aria-describedby="DescriptionMail" placeholder="Adresse mail" required>
                                         <label for="LabelMail">Adresse mail</label>
-                                        <input name="Mail" type="email" class="form-control" id="LabelMail" aria-describedby="DescriptionMail">
                                         <small id="DescriptionMail" class="form-text text-muted">Votre adresse mail nous permet de vous transmet toutes les informations vous concernant.</small>
                                     </div>
+                                    <br>
                                 </div>
                                 <div class="col-md-12 text-center">
                                     <button type="submit" class="btn btn-primary">Enregistrer</button>
