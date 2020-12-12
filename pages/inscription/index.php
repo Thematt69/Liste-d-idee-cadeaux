081988c6ff63509bdf59b68edfec453ceeb1cb36
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-12 15:51:05 +0100

    LDC-40 #start

---
+++ pages/inscription/index.php
@@ -0,0 +1,46 @@
+<?php include('script.php'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Liste d'idée cadeaux - Inscription</title>
+
+    <!-- Import -->
+    <?php include('../../widgets/import/index.php'); ?>
+</head>
+
+<body>
+
+    <?php include('../../widgets/navbar/index.php'); ?>
+
+    <div class="container">
+        <div class="row  justify-content-center">
+            <div class="col-sm-12 col-md-9 col-lg-6">
+                <br>
+                <h1 class="text-center">Inscription</h1>
+                <br>
+                <div class="card">
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="row">
+                                <div class="col-md-12">
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <br>
+                <div class="text-center">
+                    <a href="https://family.matthieudevilliers.fr/pages/connexion/">
+                        Déja un compte ? Je me connecte
+                    </a>
+                </div>
+                <br>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
