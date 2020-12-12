3a590252395254f0d908e55c15eecefb82bf7959
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-12 13:35:42 +0100

    LDC-49 #finish : Navbar mobile

--- widgets/navbar/index.php
+++ widgets/navbar/index.php
@@ -1,19 +1,32 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-secondary" id="ftco-navbar">
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
     <div class="container">
         <a class="navbar-brand" href="https://family.matthieudevilliers.fr">
             Liste d'idée cadeaux
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="ftco-nav">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item d-flex  align-items-center">
-                    <a class="nav-link d-flex" href="https://family.matthieudevilliers.fr">Mes listes</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://family.matthieudevilliers.fr">Mes listes</a>
                 </li>
-                <li class="nav-item d-flex  align-items-center">
-                    <a class="nav-link d-flex" href="https://family.matthieudevilliers.fr/pages/compte/">Mon compte</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://family.matthieudevilliers.fr/pages/compte/">Mon compte</a>
                 </li>
+                <!-- <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li> -->
             </ul>
         </div>
     </div>
