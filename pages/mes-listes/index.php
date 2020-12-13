99884eb9f8863a833ee84801378f24fe5c9f06d6
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-13 21:30:28 +0100

    LDC-25 #finish

--- pages/mes-listes/index.php
+++ pages/mes-listes/index.php
@@ -31,43 +31,60 @@
                     </div>
                     <?php
 
-                    $sql = "SELECT nom, lien_partage
+                    $sql = 'SELECT lic_liste.nom as nom, lic_liste.lien_partage as lien_partage,lic_autorisation.type as roles,lic_liste.id as id
                             FROM lic_liste
                             INNER JOIN lic_autorisation ON lic_autorisation.id_liste = lic_liste.id
-                            WHERE lic_autorisation.id_compte = 1 AND lic_autorisation.type = 'proprietaire'";
+                            WHERE lic_autorisation.id_compte = 1
+                            ORDER BY lic_liste.created_to DESC';
 
                     $response = $bdd->prepare($sql);
                     $response->execute();
 
                     while ($donnees = $response->fetch()) {
+                        if ($donnees['roles'] == 'proprietaire') {
                     ?>
-                        <div class="col-md-4">
-                            <div class="card text-dark bg-light" style="min-height: 6rem;">
-                                <a href="https://family.matthieudevilliers.fr/pages/idees/?liste=<?php echo $donnees['lien_partage'] ?>" class="stretched-link"></a>
-                                <div class="card-body d-flex align-items-center justify-content-center">
-                                    <p class="card-text"><?php echo $donnees['nom'] ?></p>
+                            <div class="col-md-4">
+                                <div class="card text-dark bg-light" style="min-height: 6rem;">
+                                    <a href="https://family.matthieudevilliers.fr/pages/idees/?liste=<?php echo $donnees['lien_partage'] ?>" class="stretched-link"></a>
+                                    <div class="card-body d-flex align-items-center justify-content-center">
+                                        <p class="card-text"><?php echo $donnees['nom'] ?></p>
+                                    </div>
                                 </div>
+                                <br>
                             </div>
-                            <br>
-                        </div>
+                        <?php
+                        } else {
+
+                            $sql1 = 'SELECT lic_compte.prenom as prenom
+                                    FROM lic_compte
+                                    INNER JOIN lic_autorisation ON lic_autorisation.id_compte = lic_compte.id
+                                    WHERE lic_autorisation.type = "proprietaire" AND lic_autorisation.id_liste = ' . $donnees['id'] . '';
+
+                            $response1 = $bdd->prepare($sql1);
+                            $response1->execute();
+
+                            $donnees1 = $response1->fetch();
+                        ?>
+                            <div class="col-md-4">
+                                <div class="card text-dark bg-light" style="min-height: 6rem;">
+                                    <a href="https://family.matthieudevilliers.fr/pages/idees/?liste=<?php echo $donnees['lien_partage'] ?>" class="stretched-link"></a>
+                                    <div class="card-body d-flex align-items-center justify-content-center">
+                                        <p class="card-text">
+                                            <?php echo $donnees['nom'] ?>
+                                            <br>
+                                            <small class="text-muted d-flex justify-content-center">de <?php echo $donnees1['prenom'] ?></small>
+                                        </p>
+                                    </div>
+                                </div>
+                                <br>
+                            </div>
                     <?php
+                            $response1->closeCursor();
+                        }
                     }
                     $response->closeCursor();
                     ?>
 
-                    <!-- <div class="col-md-4">
-                        <div class="card text-dark bg-light" style="min-height: 6rem;">
-                            <a href="https://family.matthieudevilliers.fr/pages/idees/?liste=123456" class="stretched-link"></a>
-                            <div class="card-body d-flex align-items-center justify-content-center">
-                                <p class="card-text">
-                                    Liste Anniversaire 2019
-                                    <br>
-                                    <small class="text-muted d-flex justify-content-center" data-bs-toggle="tooltip" title="Partagée avec vous">de Mathilde</small>
-                                </p>
-                            </div>
-                        </div>
-                        <br>
-                    </div> -->
                 </div>
                 <br>
             </div>
