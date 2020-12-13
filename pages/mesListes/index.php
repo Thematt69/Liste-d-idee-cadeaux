6651e277562ea21ca9faf197035bb4b5b9170ec0
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-13 13:14:44 +0100

    LDC-15 #start

---
+++ pages/mesListes/index.php
@@ -0,0 +1,27 @@
+<?php include('script.php'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Liste d'idée cadeaux - Mes listes</title>
+
+    <!-- Import -->
+    <?php include('../../widgets/import/index.php'); ?>
+</head>
+
+<body>
+
+    <?php include('../../widgets/navbar/index.php'); ?>
+
+    <div class="container">
+        <div class="row  justify-content-center">
+            <div class="col-md-12">
+                <br>
+                <br>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
