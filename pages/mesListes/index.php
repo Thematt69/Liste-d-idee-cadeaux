a8cae1e82e4343b9d312762a9b442521e6aea1f6
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-13 16:43:03 +0100

    LDC-15 #finish

--- pages/mesListes/index.php
+++ pages/mesListes/index.php
@@ -17,6 +17,68 @@
         <div class="row  justify-content-center">
             <div class="col-md-12">
                 <br>
+                <h1 class="text-center">Mes listes</h1>
+                <br>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card text-dark bg-light" style="min-height: 6rem;">
+                            <a href="https://family.matthieudevilliers.fr/pages/modifListe/" class="stretched-link"></a>
+                            <div class="card-body d-flex align-items-center justify-content-center">
+                                <p class="card-text">Créer une nouvelle liste</p>
+                            </div>
+                        </div>
+                        <br>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card text-dark bg-light" style="min-height: 6rem;">
+                            <a href="https://family.matthieudevilliers.fr/pages/idees/?liste=123456" class="stretched-link"></a>
+                            <div class="card-body d-flex align-items-center justify-content-center">
+                                <p class="card-text">Liste Noël 2020</p>
+                            </div>
+                        </div>
+                        <br>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card text-dark bg-light" style="min-height: 6rem;">
+                            <a href="https://family.matthieudevilliers.fr/pages/idees/?liste=123456" class="stretched-link"></a>
+                            <div class="card-body d-flex align-items-center justify-content-center">
+                                <p class="card-text">Liste Anniversaire 2020</p>
+                            </div>
+                        </div>
+                        <br>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card text-dark bg-light" style="min-height: 6rem;">
+                            <a href="https://family.matthieudevilliers.fr/pages/idees/?liste=123456" class="stretched-link"></a>
+                            <div class="card-body d-flex align-items-center justify-content-center">
+                                <p class="card-text">Liste Noël 2019</p>
+                            </div>
+                        </div>
+                        <br>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card text-dark bg-light" style="min-height: 6rem;">
+                            <a href="https://family.matthieudevilliers.fr/pages/idees/?liste=123456" class="stretched-link"></a>
+                            <div class="card-body d-flex align-items-center justify-content-center">
+                                <p class="card-text">Liste Anniversaire 2019</p>
+                            </div>
+                        </div>
+                        <br>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card text-dark bg-light" style="min-height: 6rem;">
+                            <a href="https://family.matthieudevilliers.fr/pages/idees/?liste=123456" class="stretched-link"></a>
+                            <div class="card-body d-flex align-items-center justify-content-center">
+                                <p class="card-text">
+                                    Liste Anniversaire 2019
+                                    <br>
+                                    <small class="text-muted d-flex justify-content-center" data-bs-toggle="tooltip" title="Partagée avec vous">de Mathilde</small>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+                    </div>
+                </div>
                 <br>
             </div>
         </div>
