5b4572d39265028522b1cee6b545ecd2fab9ed37
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-13 12:14:47 +0100

    LDC-40 #finish

--- pages/inscription/index.php
+++ pages/inscription/index.php
@@ -15,7 +15,7 @@
 
     <div class="container">
         <div class="row  justify-content-center">
-            <div class="col-sm-12 col-md-9 col-lg-6">
+            <div class="col-md-12 col-lg-8">
                 <br>
                 <h1 class="text-center">Inscription</h1>
                 <br>
@@ -23,8 +23,55 @@
                     <div class="card-body">
                         <form action="" method="post">
                             <div class="row">
-                                <div class="col-md-12">
-
+                                <div class="col-md-6">
+                                    <div class="form-floating">
+                                        <input name="Prénom" type="text" class="form-control" id="LabelPrénom" placeholder="Prénom" required>
+                                        <label for="LabelPrénom">Prénom</label>
+                                    </div>
+                                    <br>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-floating">
+                                        <input name="Nom" type="text" class="form-control" id="LabelNom" placeholder="Nom" required>
+                                        <label for="LabelNom">Nom</label>
+                                    </div>
+                                    <br>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-floating">
+                                        <input name="MDP" type="password" class="form-control" id="LabelMDP" aria-describedby="DescriptionMDP" placeholder="Mot de passe" required>
+                                        <label for="LabelMDP">Mot de passe</label>
+                                        <small id="DescriptionMDP" class="form-text text-muted">Votre mot de passe est enregistré dans un format crypté.</small>
+                                    </div>
+                                    <br>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-floating">
+                                        <input name="Naissance" type="date" class="form-control" id="LabelNaissance" aria-describedby="DescriptionNaissance" placeholder="Date de naissance" required>
+                                        <label for="LabelNaissance">Date de naissance</label>
+                                        <small id="DescriptionNaissance" class="form-text text-muted">Votre date de naissance est demandée au personne qui souhaitent accéder à vos listes partagées en lien sécurisé.</small>
+                                    </div>
+                                    <br>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-floating">
+                                        <input name="Mail" type="email" class="form-control" id="LabelMail" aria-describedby="DescriptionMail" placeholder="Adresse mail" required>
+                                        <label for="LabelMail">Adresse mail</label>
+                                        <small id="DescriptionMail" class="form-text text-muted">Votre adresse mail nous permet de vous transmet toutes les informations vous concernant.</small>
+                                    </div>
+                                    <br>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-check">
+                                        <input name="CGU" class="form-check-input" type="checkbox" id="LabelCGU">
+                                        <label class="form-check-label" for="LabelCGU">
+                                            J'accepte les <a href="https://family.matthieudevilliers.fr/pages/cgu/">Conditions Général d'Utilisation</a>
+                                        </label>
+                                    </div>
+                                    <br>
+                                </div>
+                                <div class="col-md-12 text-center">
+                                    <button type="submit" class="btn btn-primary">S'inscrire</button>
                                 </div>
                             </div>
                         </form>
