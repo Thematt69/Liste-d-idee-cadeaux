2c7e56b1cfd469f4b07b145b84d2766e590c8b57
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-13 23:12:13 +0100

    LDC-16 #finish

--- pages/idees/index.php
+++ pages/idees/index.php
@@ -21,33 +21,52 @@ include('../../scripts/verif/index.php');
 
     <div class="container">
         <div class="row  justify-content-center">
-            <div class="col-sm-12 col-md-9 col-lg-6">
+            <div class="col-sm-12">
                 <br>
                 <h1 class="text-center">Liste Noël 2020</h1>
                 <br>
+
                 <div class="card text-dark bg-light">
                     <div class="card-body">
-                        <!-- <form action="" method="post">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-floating">
-                                        <input name="Mail" type="email" class="form-control" id="LabelMail" placeholder="name@example.com" required>
-                                        <label for="LabelMail">Adresse mail</label>
-                                    </div>
-                                    <br>
-                                    <div class="form-floating">
-                                        <input name="MDP" type="password" class="form-control" id="LabelMDP" placeholder="Mot de passe" required>
-                                        <label for="LabelMDP">Mot de passe</label>
-                                    </div>
-                                    <br>
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-primary">Se connecter</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form> -->
+                        <div class="table-responsive">
+                            <table class="table table-light text-center align-middle">
+                                <thead>
+                                    <tr>
+                                        <th class="col-4">Nom</th>
+                                        <th>Lien</th>
+                                        <th>Image</th>
+                                        <th>Déja acheté</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Casque audio</td>
+                                        <td><a href="https://moncasque.fr">https://moncasque.fr</a></td>
+                                        <td>Aucune</td>
+                                        <td><input class="form-check-input" type="checkbox" checked disabled></td>
+                                        <td><button class="btn btn-outline-secondary" type="button">Modifier</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Casque audio</td>
+                                        <td><a href="https://moncasque.fr">https://moncasque.fr</a></td>
+                                        <td>Aucune</td>
+                                        <td><input class="form-check-input" type="checkbox" disabled></td>
+                                        <td><button class="btn btn-outline-secondary" type="button">Modifier</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Casque audio</td>
+                                        <td><a href="https://moncasque.fr">https://moncasque.fr</a></td>
+                                        <td>Aucune</td>
+                                        <td><input class="form-check-input" type="checkbox" checked disabled></td>
+                                        <td><button class="btn btn-outline-secondary" type="button" disabled>Modifier</button></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
+
                 <br>
             </div>
         </div>
