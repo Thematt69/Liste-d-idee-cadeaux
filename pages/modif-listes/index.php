fac1295dd6dfbae2ca407c3a6e24c86af18cc52c
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-14 18:43:38 +0100

    LDC-17 #finish

--- pages/modif-listes/index.php
+++ pages/modif-listes/index.php
@@ -29,6 +29,29 @@ include('../../scripts/verif/index.php');
                     <div class="card-body">
                         <form action="" method="post">
                             <div class="row">
+                                <div class="col-md-8">
+                                    <div class="form-floating">
+                                        <input name="Nom de la liste" type="text" class="form-control" id="LabelNom" placeholder="Nom de la liste" required>
+                                        <label for="LabelNom">Nom de la liste</label>
+                                    </div>
+                                    <br>
+                                </div>
+                                <div class="col-md-4">
+                                    <select class="form-select" name="partage" aria-label="Paramètres de partage">
+                                        <option value="prive">Privé</option>
+                                        <option value="lien">Partagée par lien</option>
+                                        <option value="secure">Partagée par lien (sécurisé)</option>
+                                    </select>
+                                    <br>
+                                </div>
+                                <div class="col-6 col-md-4 text-center">
+                                    <button type="submit" class="btn btn-danger">Supprimer</button>
+                                    <br>
+                                </div>
+                                <div class="col-6 col-md-4 text-center">
+                                    <button type="submit" class="btn btn-primary">Enregistrer</button>
+                                    <br>
+                                </div>
                             </div>
                         </form>
                     </div>
