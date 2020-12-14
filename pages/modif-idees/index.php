eb84cc63f0f17b2531cca0cff0164cb98009fae2
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-14 20:19:17 +0100

    LDC-18 #start

---
+++ pages/modif-idees/index.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+
+include('../../scripts/verif/index.php');
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Liste d'idée cadeaux - Création / Modification d'idée</title>
+
+    <!-- Import -->
+    <?php include('../../widgets/import/index.php'); ?>
+</head>
+
+<body>
+
+    <?php include('../../widgets/navbar/index.php'); ?>
+
+    <div class="container">
+        <div class="row  justify-content-center">
+            <div class="col-md-12 col-lg-8">
+                <br>
+                <h1 class="text-center">Création / Modification d'idée</h1>
+                <br>
+                <div class="card">
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="row">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <br>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
