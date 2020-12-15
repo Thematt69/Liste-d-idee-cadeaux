91799716c00698df56e6a4412ff91537012a06d3
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-15 22:12:49 +0100

    LDC-37 #finish

--- pages/modif-listes/index.php
+++ pages/modif-listes/index.php
@@ -28,31 +28,50 @@ include('../../scripts/verif/index.php');
                 <div class="card">
                     <div class="card-body">
                         <form action="" method="post">
+                            <?php
+
+                            $sql = 'SELECT nom,partage
+                                    FROM lic_liste
+                                    WHERE lien_partage = "' . $_GET['liste'] . '"';
+
+                            $response = $bdd->prepare($sql);
+                            $response->execute();
+
+                            $donnees = $response->fetch();
+
+                            ?>
                             <div class="row">
                                 <div class="col-md-8">
                                     <div class="form-floating">
-                                        <input name="Nom de la liste" type="text" class="form-control" id="LabelNom" placeholder="Nom de la liste" required>
+                                        <input name="Nom de la liste" value="<?php echo $donnees['nom'] ?>" type="text" class="form-control" id="LabelNom" placeholder="Nom de la liste" required>
                                         <label for="LabelNom">Nom de la liste</label>
                                     </div>
                                     <br>
                                 </div>
                                 <div class="col-md-4">
                                     <select class="form-select" name="partage" aria-label="Paramètres de partage">
-                                        <option value="prive">Privé</option>
-                                        <option value="lien">Partagée par lien</option>
-                                        <option value="secure">Partagée par lien (sécurisé)</option>
+                                        <option <?php if ($donnees['partage'] == 'prive') echo ('selected') ?> value="prive">Privé</option>
+                                        <option <?php if ($donnees['partage'] == 'lien') echo ('selected') ?> value="lien">Partagée par lien</option>
+                                        <option <?php if ($donnees['partage'] == 'secure') echo ('selected') ?> value="secure">Partagée par lien (sécurisé)</option>
                                     </select>
-                                    <br>
-                                </div>
-                                <div class="col-6 col-md-4 text-center">
-                                    <button type="button" class="btn btn-danger">Supprimer</button>
                                     <br>
                                 </div>
                                 <div class="col-6 col-md-4 text-center">
                                     <button type="submit" class="btn btn-primary">Enregistrer</button>
                                     <br>
                                 </div>
+                                <?php
+                                if (isset($_GET['liste'])) {
+                                ?>
+                                    <div class="col-6 col-md-4 text-center">
+                                        <button type="button" class="btn btn-danger">Supprimer</button>
+                                        <br>
+                                    </div>
+                                <?php
+                                }
+                                ?>
                             </div>
+                            <?php $response->closeCursor(); ?>
                         </form>
                     </div>
                 </div>
