5ab39d1e118661a57e4d7468024d62e499b38cee
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-15 22:26:51 +0100

    LDC-26 #refinish

--- pages/idees/index.php
+++ pages/idees/index.php
@@ -30,100 +30,114 @@ include('../../scripts/verif/index.php');
     $donnee = $response->fetch();
 
     if ($donnee == null) {
-        $donnee['nom'] = 'Liste introuvable';
-        $donnee['id'] = 0;
+    ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <br>
+                    <div class="alert alert-danger" role="alert">
+                        <strong>Liste introuvable !</strong> Nous sommes désolées, mais nous n'avons pas trouvé votre liste. Merci de revenir à <a href="https://family.matthieudevilliers.fr/pages/mes-listes/" class="alert-link">vos listes</a>.
+                    </div>
+                    <br>
+                </div>
+            </div>
+        </div>
+
+    <?php
+    } else {
+    ?>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-12">
+                    <br>
+                    <h1 class="text-center">
+                        <?php
+                        switch ($donnee['partage']) {
+                            case 'lien':
+                                echo ('<i class="fas fa-users fa-2x" style="font-size: 1.5rem;"></i>');
+                                break;
+
+                            case 'secure':
+                                echo ('<i class="fas fa-user-lock fa-2x" style="font-size: 1.5rem;"></i>');
+                                break;
+
+                            default:
+                                echo ('<i class="fas fa-lock fa-2x" style="font-size: 1.5rem;"></i>');
+                                break;
+                        }
+                        ?>
+                        <?php echo $donnee['nom'] ?>
+                        <a href="https://family.matthieudevilliers.fr/pages/modif-listes/?liste=<?php echo ($_GET['liste']) ?>" class="link-dark">
+                            <i class="fas fa-pen fa-2x" style="font-size: 1.5rem;"></i>
+                        </a>
+                    </h1>
+                    <br>
+
+                    <div class="card text-dark bg-light">
+                        <div class="card-body text-center">
+                            <div class="table-responsive">
+                                <table class="table table-light text-center align-middle">
+                                    <thead>
+                                        <tr>
+                                            <th class="col">Nom</th>
+                                            <th class="col lien">Lien</th>
+                                            <th class="col image">Image</th>
+                                            <th class="col">Déja acheté</th>
+                                            <th class="col"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+
+                                        $sql1 = 'SELECT id,nom,lien,image,is_buy
+                                            FROM lic_idee
+                                            WHERE id_liste = ' . $donnee['id'] . '';
+
+                                        $response1 = $bdd->prepare($sql1);
+                                        $response1->execute();
+
+                                        while ($donnees = $response1->fetch()) {
+                                        ?>
+                                            <tr>
+                                                <td><?php echo ($donnees['nom']) ?></td>
+                                                <td class="lien"><a href="<?php echo ($donnees['lien']) ?>"><?php if ($donnees['lien'] != '') echo (substr($donnees['lien'], 0, 40) . '...') ?></a></td>
+                                                <td class="image">
+                                                    <?php if ($donnees['image'] == null) {
+                                                        echo ('Aucune');
+                                                    } else {
+                                                    ?>
+                                                        <a href="https://family.matthieudevilliers.fr/images/<?php echo ($donnees['image']) ?>">
+                                                            <?php echo ($donnees['image']) ?>
+                                                        </a>
+                                                    <?php
+                                                    }
+                                                    ?>
+                                                </td>
+                                                <td><input class="form-check-input" type="checkbox" <?php if ($donnees['is_buy']) echo ('checked') ?> disabled></td>
+                                                <td><a class="btn btn-outline-secondary" href="https://family.matthieudevilliers.fr/pages/modif-idees/?idee=<?php echo ($donnees['id']) ?>">Modifier</a></td>
+                                            </tr>
+                                        <?php
+                                        }
+
+                                        $response1->closeCursor();
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <a class="btn btn-primary" href="https://family.matthieudevilliers.fr/pages/modif-idees/">Ajouter une idée</a>
+                        </div>
+                    </div>
+
+                    <br>
+                </div>
+            </div>
+        </div>
+    <?php
     }
 
     $response->closeCursor();
 
     ?>
-
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-sm-12">
-                <br>
-                <h1 class="text-center">
-                    <?php
-                    switch ($donnee['partage']) {
-                        case 'lien':
-                            echo ('<i class="fas fa-users fa-2x" style="font-size: 1.5rem;"></i>');
-                            break;
-
-                        case 'secure':
-                            echo ('<i class="fas fa-user-lock fa-2x" style="font-size: 1.5rem;"></i>');
-                            break;
-
-                        default:
-                            echo ('<i class="fas fa-lock fa-2x" style="font-size: 1.5rem;"></i>');
-                            break;
-                    }
-                    ?>
-                    <?php echo $donnee['nom'] ?>
-                    <a href="https://family.matthieudevilliers.fr/pages/modif-listes/?liste=<?php echo ($_GET['liste']) ?>" class="link-dark">
-                        <i class="fas fa-pen fa-2x" style="font-size: 1.5rem;"></i>
-                    </a>
-                </h1>
-                <br>
-
-                <div class="card text-dark bg-light">
-                    <div class="card-body text-center">
-                        <div class="table-responsive">
-                            <table class="table table-light text-center align-middle">
-                                <thead>
-                                    <tr>
-                                        <th class="col">Nom</th>
-                                        <th class="col lien">Lien</th>
-                                        <th class="col image">Image</th>
-                                        <th class="col">Déja acheté</th>
-                                        <th class="col"></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-
-                                    $sql1 = 'SELECT id,nom,lien,image,is_buy
-                                            FROM lic_idee
-                                            WHERE id_liste = ' . $donnee['id'] . '';
-
-                                    $response1 = $bdd->prepare($sql1);
-                                    $response1->execute();
-
-                                    while ($donnees = $response1->fetch()) {
-                                    ?>
-                                        <tr>
-                                            <td><?php echo ($donnees['nom']) ?></td>
-                                            <td class="lien"><a href="<?php echo ($donnees['lien']) ?>"><?php if ($donnees['lien'] != '') echo (substr($donnees['lien'], 0, 40) . '...') ?></a></td>
-                                            <td class="image">
-                                                <?php if ($donnees['image'] == null) {
-                                                    echo ('Aucune');
-                                                } else {
-                                                ?>
-                                                    <a href="https://family.matthieudevilliers.fr/images/<?php echo ($donnees['image']) ?>">
-                                                        <?php echo ($donnees['image']) ?>
-                                                    </a>
-                                                <?php
-                                                }
-                                                ?>
-                                            </td>
-                                            <td><input class="form-check-input" type="checkbox" <?php if ($donnees['is_buy']) echo ('checked') ?> disabled></td>
-                                            <td><a class="btn btn-outline-secondary" href="https://family.matthieudevilliers.fr/pages/modif-idees/?idee=<?php echo ($donnees['id']) ?>">Modifier</a></td>
-                                        </tr>
-                                    <?php
-                                    }
-
-                                    $response1->closeCursor();
-                                    ?>
-                                </tbody>
-                            </table>
-                        </div>
-                        <a class="btn btn-primary" href="https://family.matthieudevilliers.fr/pages/modif-idees/">Ajouter une idée</a>
-                    </div>
-                </div>
-
-                <br>
-            </div>
-        </div>
-    </div>
 
 </body>
 
