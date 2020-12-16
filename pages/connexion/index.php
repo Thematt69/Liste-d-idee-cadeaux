17cc570fcf0100dabc3bfb0214055703c58a1bc5
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-16 22:46:00 +0100

    LDC-47 #finish

--- pages/connexion/index.php
+++ pages/connexion/index.php
@@ -4,6 +4,31 @@ session_start();
 
 include('../../scripts/verif/index.php');
 
+$alert = false;
+
+if ($_POST['Mail']) {
+
+    $sql = 'SELECT id,motdepasse
+            FROM lic_compte
+            WHERE mail = ?';
+
+    $response = $bdd->prepare($sql);
+    $response->execute(array($_POST['Mail']));
+
+    $donnee = $response->fetch();
+
+    if (password_verify($_POST['MDP'], $donnee['motdepasse'])) {
+        // Le mot de passe correspond
+        $_SESSION['id_compte'] = $donnee['id'];
+        header('Location: https://family.matthieudevilliers.fr/pages/mes-listes/');
+    } else {
+        // Le mot de passe ne correspond pas
+        $alert = true;
+    }
+
+    $response->closeCursor();
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -23,6 +48,17 @@ include('../../scripts/verif/index.php');
         <div class="row  justify-content-center">
             <div class="col-sm-12 col-md-9 col-lg-6">
                 <br>
+                <?php
+                if ($alert) {
+                ?>
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Mot de passe incorrect !</strong>
+                        Votre mail ou votre mot de passe est incorrect, merci de réessayer.
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                <?php
+                }
+                ?>
                 <h1 class="text-center">Connexion</h1>
                 <br>
                 <div class="card">
