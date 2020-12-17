fcd29042ac624f128be86050c681ff1544825472
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-17 20:35:43 +0100

    LDC-42 #finish

--- pages/inscription/index.php
+++ pages/inscription/index.php
@@ -4,6 +4,64 @@ session_start();
 
 include('../../scripts/verif/index.php');
 
+$alert = false;
+
+if (isset($_POST['Mail'])) {
+
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+    $data = array('secret' => '6Lc-ZgkaAAAAAAcf5v9GqcfTfm2o6lzOq1Y6kftU', 'response' => $_POST['g-recaptcha-response']);
+
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result !== FALSE) {
+        $response = json_decode($result);
+    }
+
+    if ($response->success) {
+
+        $sql = 'SELECT mail
+            FROM lic_compte
+            WHERE mail = ?';
+
+        $response = $bdd->prepare($sql);
+        $response->execute(array(htmlentities($_POST['Mail'])));
+
+        $donnee = $response->fetch();
+
+        if (!isset($donnee['mail'])) {
+
+            $_SESSION['prenom'] = htmlentities($_POST['Prénom']);
+            $_SESSION['nom'] = htmlentities($_POST['Nom']);
+            $_SESSION['motdepasse'] = password_hash(htmlentities($_POST['MDP']), PASSWORD_DEFAULT);
+            $date = new DateTime(htmlentities($_POST['Naissance']));
+            $_SESSION['date_naissance'] = $date->format('Y-m-d');
+            $_SESSION['mail'] = htmlentities($_POST['Mail']);
+
+            $sql1 = 'INSERT INTO lic_compte (prenom,nom,motdepasse,date_naissance,mail)
+                VALUES (?,?,?,?,?)';
+
+            $response1 = $bdd->prepare($sql1);
+            $response1->execute(array($_SESSION['prenom'], $_SESSION['nom'], $_SESSION['motdepasse'], $_SESSION['date_naissance'], $_SESSION['mail']));
+            $response1->closeCursor();
+
+            header('Location: https://family.matthieudevilliers.fr/pages/mes-listes/');
+        } else {
+            $alert = "L'adresse mail est déja utilisé !";
+        }
+
+        $response->closeCursor();
+    } else {
+        $alert = 'CAPTCHA invalide !';
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -13,6 +71,8 @@ include('../../scripts/verif/index.php');
 
     <!-- Import -->
     <?php include('../../widgets/import/index.php'); ?>
+
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 
 <body>
@@ -23,6 +83,16 @@ include('../../scripts/verif/index.php');
         <div class="row  justify-content-center">
             <div class="col-md-12 col-lg-8">
                 <br>
+                <?php
+                if ($alert) {
+                ?>
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong><?php echo $alert; ?></strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                <?php
+                }
+                ?>
                 <h1 class="text-center">Inscription</h1>
                 <br>
                 <div class="card">
@@ -69,10 +139,14 @@ include('../../scripts/verif/index.php');
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-check">
-                                        <input name="CGU" class="form-check-input" type="checkbox" id="LabelCGU">
+                                        <input name="CGU" class="form-check-input" type="checkbox" id="LabelCGU" required>
                                         <label class="form-check-label" for="LabelCGU">
                                             J'accepte les <a href="https://family.matthieudevilliers.fr/pages/cgu/">Conditions Général d'Utilisation</a>
                                         </label>
+                                    </div>
+                                    <br>
+                                    <div class="d-flex justify-content-center">
+                                        <div class="g-recaptcha" data-sitekey="6Lc-ZgkaAAAAAEoCEsUwvVPygJPyhxGDtPIvkppO"></div>
                                     </div>
                                     <br>
                                 </div>
