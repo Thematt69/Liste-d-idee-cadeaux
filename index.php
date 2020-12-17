992649dd5be6593c29761170047b8c32d1688fa2
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-17 20:37:54 +0100

    LDC-66 #finish

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-header('Location: https://family.matthieudevilliers.fr/pages/mes-listes/');
+
+session_start();
+
+include('../../scripts/verif/index.php');
+
+if (isset($_SESSION['mail'])) {
+    header('Location: https://family.matthieudevilliers.fr/pages/mes-listes/');
+} else {
+    header('Location: https://family.matthieudevilliers.fr/pages/connexion/');
+}
