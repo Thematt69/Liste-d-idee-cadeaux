6981ea29c5c339479f433e9eb90d4a753a75a0fc
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2020-12-19 14:18:00 +0100

    LDC-74 #finish

--- widgets/navbar/index.php
+++ widgets/navbar/index.php
@@ -1,18 +1,23 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+<nav class="navbar navbar-expand-md navbar-dark bg-secondary shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="https://family.matthieudevilliers.fr">
+            <img src="https://family.matthieudevilliers.fr/images/logo.webp" width="30" height="30" class="d-inline-block align-top" alt="Logo du site" loading="lazy">
             Liste d'idée cadeaux
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="https://family.matthieudevilliers.fr/pages/mes-listes/">Mes listes</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="https://family.matthieudevilliers.fr/pages/compte/">Mon compte</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://family.matthieudevilliers.fr/scripts/deconnexion/">Déconnexion</a>
                 </li>
                 <!-- <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
