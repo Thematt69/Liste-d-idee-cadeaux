a3ba33cabfbd4434d0300bfff0d9f034d0004992
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-01-24 00:28:40 +0100

    Gestion erreur page

---
+++ pages/erreur/403.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Liste d'idée cadeaux - Mon compte</title>
+
+    <!-- Import -->
+    <?php include('../../widgets/import/index.php'); ?>
+</head>
+
+<body>
+
+    <?php include('../../widgets/navbar/index.php'); ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <br>
+                <h1 class="text-center">Erreur 403</h1>
+                <br>
+                <div class="text-center">
+                    <h4>Vous n'êtes pas autorisé à accéder à ce fichier.</h4>
+                    <br>
+                    <a class="btn btn-outline-primary" href="https://family.matthieudevilliers.fr">
+                        Revenir au site
+                    </a>
+                    <br>
+                    <video autoplay loop muted style="max-width: 100%;">
+                        <source src="https://spa.matthieudevilliers.fr/images/maintenance-resize.webm" type="video/webm">
+                        <source src="https://spa.matthieudevilliers.fr/images/maintenance-resize.mp4" type="video/mp4">
+                        <p>Votre navigateur ne prend pas en charge les vidéos HTML5.</p>
+                    </video>
+                </div>
+                <br>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
