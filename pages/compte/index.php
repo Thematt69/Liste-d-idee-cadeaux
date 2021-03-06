7332636bf4cea2d93e9d1f1a3597c2d2003f735a
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-03-06 13:36:02 +0100

    LDC-89 #finish

--- pages/compte/index.php
+++ pages/compte/index.php
@@ -11,6 +11,8 @@ if (!isset($_SESSION['id_compte'])) {
 $alert = false;
 $info = false;
 
+$dateTimeNow = new DateTime();
+
 if (isset($_POST['Mail'])) {
 
     if (($_POST['NewMDP'] != $_POST['MDP'])) {
@@ -138,7 +140,7 @@ if (isset($_POST['Mail'])) {
                                 </div>
                                 <div class="col-md-6 col-xl-4">
                                     <div class="form-floating">
-                                        <input name="Naissance" type="date" class="form-control" id="LabelNaissance" aria-describedby="DescriptionNaissance" placeholder="Date de naissance" value="<?php echo $donnees['date_naissance'] ?>" required>
+                                        <input name="Naissance" type="date" class="form-control" id="LabelNaissance" aria-describedby="DescriptionNaissance" placeholder="Date de naissance" value="<?php echo $donnees['date_naissance'] ?>" min="<?php echo $dateTimeSub1Year->format('Y-m-d') ?>" max="<?php echo $dateTimeNow->format('Y-m-d') ?>" required>
                                         <label for="LabelNaissance">Date de naissance</label>
                                         <small id="DescriptionNaissance" class="form-text text-muted">Votre date de naissance est un moyen de sécuriser les listes que vous partagez.</small>
                                     </div>
