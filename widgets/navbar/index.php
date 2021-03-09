5aea9ed0e735392a705c97d8d55df5b46e42ef3c
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-03-09 20:07:34 +0100

    début alerte

--- widgets/navbar/index.php
+++ widgets/navbar/index.php
@@ -33,20 +33,67 @@
                     </li>
                 <?php
                 }
+
+                $sqlreq3 = 'SELECT count(id)
+                            FROM lic_notif
+                            WHERE id_compte = ? AND etat = "non-lu"';
+
+                $req3 = $bdd->prepare($sqlreq3);
+                $req3->execute(array($_SESSION['id_compte']));
+                $donneesreq3 = $req3->fetch();
+
                 ?>
-                <!-- <li class="nav-item dropdown">
+                <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                        <?php
+                        if ($donneesreq3['count(id)'] > 0) echo ('<i class="fas fa-bell" style="color: Tomato;"></i>');
+                        else echo ('<i class="far fa-bell"></i>');
+                        ?>
                     </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <ul class="dropdown-menu text-center" aria-labelledby="navbarDropdown">
+                        <?php
+
+                        $sqlreq2 = 'SELECT titre,message,etat,created_to
+                                        FROM lic_notif
+                                        WHERE id_compte = ?';
+
+                        $req2 = $bdd->prepare($sqlreq2);
+                        $req2->execute(array($_SESSION['id_compte']));
+
+
+                        while ($donneesreq2 = $req2->fetch()) {
+                            $datetime = new DateTime($donneesreq2["created_to"]);
+                        ?>
+                            <li>
+                                <strong><?php echo $donneesreq2["titre"] ?></strong>
+                                <br>
+                                <?php echo $donneesreq2["message"] ?>
+                                <br>
+                                <small>Le <?php echo $datetime->format("d/m/Y H:m") ?></small>
+                            </li>
+                        <?php
+                        }
+
+                        $req2->closeCursor();
+
+                        $sqlreq1 = 'SELECT count(id)
+                                    FROM lic_notif
+                                    WHERE id_compte = ?';
+
+                        $req1 = $bdd->prepare($sqlreq1);
+                        $req1->execute(array($_SESSION['id_compte']));
+                        $donneesreq1 = $req1->fetch();
+
+                        if ($donneesreq1['count(id)'] == 0) echo ('<li>Aucune alerte</li>');
+
+                        $req1->closeCursor();
+                        ?>
                     </ul>
-                </li> -->
+                </li>
+                <?php
+
+                $req3->closeCursor();
+                ?>
             </ul>
         </div>
     </div>
