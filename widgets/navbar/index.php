799b6cdf823a677ce83e1720e6b9cf941951438c
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-03-10 19:58:36 +0100

    Notif Alerte

--- widgets/navbar/index.php
+++ widgets/navbar/index.php
@@ -11,8 +11,103 @@
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ms-auto">
                 <?php
+
+                if (isset($_POST['notif'])) {
+                    if ($_POST['notif'] == "tous") {
+                        $sqlreq6 = 'UPDATE lic_notif 
+                                    SET etat = "lu"
+                                    WHERE id_compte = ? AND etat != "lu"';
+
+                        $req6 = $bdd->prepare($sqlreq6);
+                        $req6->execute(array($_SESSION['id_compte']));
+
+                        $req6->closeCursor();
+                    } else {
+                        $sqlreq6 = 'UPDATE lic_notif 
+                                    SET etat = "lu"
+                                    WHERE id_compte = ? AND etat != "lu" AND id = ?';
+
+                        $req6 = $bdd->prepare($sqlreq6);
+                        $req6->execute(array($_SESSION['id_compte'], $_POST['notif']));
+
+                        $req6->closeCursor();
+                    }
+                }
+
                 if (isset($_SESSION['id_compte'])) {
                 ?>
+                    <?php
+
+                    $sqlreq2 = 'SELECT count(id) as num_alert
+                                        FROM lic_notif
+                                        WHERE id_compte = ? AND etat = "non-lu"';
+
+                    $req2 = $bdd->prepare($sqlreq2);
+                    $req2->execute(array($_SESSION['id_compte']));
+                    $donneesreq2 = $req2->fetch();
+
+                    // NOTE - Si il y a des alertes non-lus
+                    if ($donneesreq2['num_alert'] > 0) {
+                    ?>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="far fa-bell"></i>
+                            </a>
+                            <ul class="dropdown-menu text-center" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <form action="" method="post">
+                                        <small>
+                                            <button name="notif" value="tous" class="btn btn-link btn-sm" type="submit">
+                                                Marquer tout comme lu
+                                            </button>
+                                        </small>
+                                    </form>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <?php
+
+                                $sqlreq3 = 'SELECT id,titre,message,etat,created_to
+                                            FROM lic_notif
+                                            WHERE id_compte = ? AND etat = "non-lu"';
+
+                                $req3 = $bdd->prepare($sqlreq3);
+                                $req3->execute(array($_SESSION['id_compte']));
+
+                                // TODO - Afficher les alertes non-lus
+                                while ($donneesreq3 = $req3->fetch()) {
+                                    $datetime = new DateTime($donneesreq3["created_to"]);
+                                ?>
+                                    <li>
+                                        <form action="" method="post">
+                                            <strong>
+                                                <?php echo $donneesreq3["titre"] ?>
+                                            </strong>
+                                            <br>
+                                            <?php echo $donneesreq3["message"] ?>
+                                            <br>
+                                            <small>
+                                                Le <?php echo $datetime->format("d/m/Y H:i") ?>
+                                            </small>
+                                            <small>
+                                                <button style="vertical-align: initial;" name="notif" value="<?php echo $donneesreq3["id"] ?>" class="btn btn-link btn-sm" type="submit">
+                                                    Marquer comme lu
+                                                </button>
+                                            </small>
+                                        </form>
+                                    </li>
+                                <?php
+                                }
+
+                                $req3->closeCursor();
+                                ?>
+
+                            </ul>
+                        </li>
+                    <?php
+                    }
+                    ?>
                     <li class="nav-item">
                         <a class="nav-link" href="https://family.matthieudevilliers.fr/pages/listes/">Mes listes</a>
                     </li>
@@ -23,6 +118,7 @@
                         <a class="nav-link" href="https://family.matthieudevilliers.fr/scripts/deconnexion/">Déconnexion</a>
                     </li>
                 <?php
+                    $req2->closeCursor();
                 } else {
                 ?>
                     <li class="nav-item">
@@ -33,66 +129,6 @@
                     </li>
                 <?php
                 }
-
-                $sqlreq3 = 'SELECT count(id)
-                            FROM lic_notif
-                            WHERE id_compte = ? AND etat = "non-lu"';
-
-                $req3 = $bdd->prepare($sqlreq3);
-                $req3->execute(array($_SESSION['id_compte']));
-                $donneesreq3 = $req3->fetch();
-
-                ?>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <?php
-                        if ($donneesreq3['count(id)'] > 0) echo ('<i class="fas fa-bell" style="color: Tomato;"></i>');
-                        else echo ('<i class="far fa-bell"></i>');
-                        ?>
-                    </a>
-                    <ul class="dropdown-menu text-center" aria-labelledby="navbarDropdown">
-                        <?php
-
-                        $sqlreq2 = 'SELECT titre,message,etat,created_to
-                                        FROM lic_notif
-                                        WHERE id_compte = ?';
-
-                        $req2 = $bdd->prepare($sqlreq2);
-                        $req2->execute(array($_SESSION['id_compte']));
-
-
-                        while ($donneesreq2 = $req2->fetch()) {
-                            $datetime = new DateTime($donneesreq2["created_to"]);
-                        ?>
-                            <li>
-                                <strong><?php echo $donneesreq2["titre"] ?></strong>
-                                <br>
-                                <?php echo $donneesreq2["message"] ?>
-                                <br>
-                                <small>Le <?php echo $datetime->format("d/m/Y H:m") ?></small>
-                            </li>
-                        <?php
-                        }
-
-                        $req2->closeCursor();
-
-                        $sqlreq1 = 'SELECT count(id)
-                                    FROM lic_notif
-                                    WHERE id_compte = ?';
-
-                        $req1 = $bdd->prepare($sqlreq1);
-                        $req1->execute(array($_SESSION['id_compte']));
-                        $donneesreq1 = $req1->fetch();
-
-                        if ($donneesreq1['count(id)'] == 0) echo ('<li>Aucune alerte</li>');
-
-                        $req1->closeCursor();
-                        ?>
-                    </ul>
-                </li>
-                <?php
-
-                $req3->closeCursor();
                 ?>
             </ul>
         </div>
