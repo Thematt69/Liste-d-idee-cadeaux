04cc410e5cbbf70f4c2ea6015bd0f15464e0a813
Matthieu Devilliers <60305744+Thematt69@users.noreply.github.com>
2021-05-17 10:29:33 +0200

    Delete sql directory

--- sql/bdd.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    // On se connecte à MySQL
-    $bdd = new PDO('mysql:host=185.98.131.128;dbname=matth1371558;charset=utf8', 'matth1371558', 'bejq29xnon', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-} catch (Exception $e) {
-    // En cas d'erreur, on affiche un message et on arrête tout
-    die('Erreur : ' . $e->getMessage());
-}
