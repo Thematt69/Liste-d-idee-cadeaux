6e07a7f2b225351b3fcf85519f5159a22ded7be4
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2022-10-24 15:54:27 +0200

    fix access bdd

--- scripts/verif/index.php
+++ scripts/verif/index.php
@@ -1,9 +1,12 @@
 <?php
+try {
+    if (!$_SERVER['HTTPS']) {
+        header('Location: https://family.matthieudevilliers.fr' . $_SERVER['PHP_SELF']);
+    }
 
-if (!$_SERVER['HTTPS']) {
-    header('Location: https://family.matthieudevilliers.fr' . $_SERVER['PHP_SELF'] . '');
+    // On se connecte à MySQL
+    $bdd = new PDO('mysql:host=185.98.131.128;dbname=matth1371558;charset=utf8', 'matth1371558', 'bejq29xnon', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch (Exception $e) {
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : ' . $e->getMessage());
 }
-
-// NOTE - BDD import
-
-include('../../sql/bdd.php');
