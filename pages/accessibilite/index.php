d76d1fd222f606af1eb58985015f017d7e69d801
Matthieu Devilliers <60305744+Thematt69@users.noreply.github.com>
2025-04-04 15:01:40 +0000

    add accessibilite page

---
+++ pages/accessibilite/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="fr" class="h-100">
+
+<head>
+    <title>Listes d'idées cadeau - CGU</title>
+
+    <!-- Import -->
+    <?php include('../../widgets/import/index.php'); ?>
+</head>
+
+<body class="d-flex flex-column h-100">
+
+    <?php include('../../widgets/navbar/index.php'); ?>
+
+    <main>
+
+        <h1>Déclaration d’accessibilité</h1>
+
+        <p><strong>TheMatt69</strong> s’engage à rendre ses sites internet, intranet, extranet et ses progiciels accessibles (et ses applications mobiles et mobilier urbain numérique) conformément à l’article 47 de la loi n°2005-102 du 11 février 2005.</p>
+
+        <p>Cette déclaration d’accessibilité s’applique à <strong>https://family.matthieudevilliers.fr/</strong>.</p>
+
+        <h2>État de conformité</h2>
+
+        <p><strong>Listes d'idées cadeau <a target="_blank" href="https://family.matthieudevilliers.fr/">https://family.matthieudevilliers.fr/</a></strong> est <strong>partiellement conforme</strong> avec le référentiel général d’amélioration de l’accessibilité (RGAA).</p>
+
+        <h2>Résultats des tests</h2>
+
+        <p>L’audit de conformité réalisé par <strong>TheMatt69</strong> révèle que <strong>73%</strong> des critères du RGAA version 4.1.2 sont respectés.</p>
+
+        <h2> Établissement de cette déclaration d’accessibilité </h2>
+
+        <p>Cette déclaration a été établie le <strong>4 avril 2025</strong>. Elle a été mise à jour le <strong>4 avril 2025</strong>.</p>
+
+        <h3> Technologies utilisées pour la réalisation de l’audit </h3>
+
+        <ul>
+            <li>HTML5</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+        </ul>
+
+        <h3>Environnement de test</h3>
+
+        <p>Les vérifications de restitution de contenus ont été réalisées sur la base de la combinaison fournie par la base de référence du RGAA, avec les versions suivantes :</p>
+
+        <ul>
+            <li> Sur Ordinateur macOS avec Google Chrome et VoiceOver</li>
+        </ul>
+
+        <h3>Outils pour évaluer l’accessibilité</h3>
+
+        <ul>
+            <li>Inspecteur de composants</li>
+            <li>Colour Contrast Analyser</li>
+        </ul>
+
+        <h3> Pages du site ayant fait l’objet de la vérification de conformité </h3>
+
+        <ul>
+            <li>Connexion <strong>https://family.matthieudevilliers.fr/pages/connexion/</strong></li>
+            <li>Mes listes <strong>https://family.matthieudevilliers.fr/pages/listes/</strong></li>
+            <li>Mon compte <strong>https://family.matthieudevilliers.fr/pages/compte/</strong></li>
+            <li>Créer une liste <strong>https://family.matthieudevilliers.fr/pages/modif-listes/?type=solo</strong></li>
+            <li>Idées de la liste <strong>https://family.matthieudevilliers.fr/pages/idees/?liste=156aeeb8ed16c7dcfad6c9c3f42c5c4b</strong></li>
+            <li>Modification d'une idée <strong>https://family.matthieudevilliers.fr/pages/modif-idees/?idee=32</strong></li>
+        </ul>
+
+        <h2>Retour d’information et contact</h2>
+
+        <p>Si vous n’arrivez pas à accéder à un contenu ou à un service, vous pouvez contacter le responsable de Listes d'idées cadeau pour être orienté vers une alternative accessible ou obtenir le contenu sous une autre forme.</p>
+
+        <ul>
+            <li> Contacter <strong>TheMatt69 : webmaster@matthieudevilliers.fr</strong></li>
+        </ul>
+
+        <h2>Voies de recours</h2>
+
+        <p>Si vous constatez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une fonctionnalité du site, que vous nous le signalez et que vous ne parvenez pas à obtenir une réponse de notre part, vous êtes en droit de faire parvenir vos doléances ou une demande de saisine au Défenseur des droits.</p>
+
+        <p>Plusieurs moyens sont à votre disposition :</p>
+
+        <ul>
+            <li><a href="https://formulaire.defenseurdesdroits.fr/formulaire_saisine" target="_blank"> Écrire un message au Défenseur des droits <span>nouvelle fenêtre</span></a></li>
+            <li><a href="https://www.defenseurdesdroits.fr/carte-des-delegues" target="_blank"> Contacter le délégué du Défenseur des droits dans votre région <span>nouvelle fenêtre</span></a></li>
+            <li> Envoyer un courrier par la poste (gratuit, ne pas mettre de timbre) à :<br><span>Défenseur des droits<br> Libre réponse 71120<br> 75342 Paris CEDEX 07</span></li>
+        </ul>
+    </main>
+
+</body>
+
+<?php include('../../widgets/footer/index.php'); ?>
+
+</html>
