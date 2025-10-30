cbffdce3ea899aebc8d1c6e63b966799ddad2442
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2025-10-30 22:41:42 +0100

    fix: update copyright year in footer to reflect 2020-2025

--- widgets/footer/index.php
+++ widgets/footer/index.php
@@ -1,12 +1,12 @@
 <footer class="footer mt-auto py-3 bg-secondary">
     <div class="container text-light text-center">
         <span>
-            <a href="https://family.matthieudevilliers.fr/pages/accessibilite/" target="_blank" class="link-light">Accessibilité : partiellement conforme</a>
+            <a href="https://family.matthieudevilliers.fr/pages/accessibilite/" class="link-light">Accessibilité : partiellement conforme</a>
             |
-            <a href="https://family.matthieudevilliers.fr/pages/cgu/" target="_blank" class="link-light">CGU</a>
+            <a href="https://family.matthieudevilliers.fr/pages/cgu/" class="link-light">CGU</a>
             |
             <a href="https://matthieudevilliers.fr/" target="_blank" class="link-light">Matthieu Devilliers</a>
-            | Copyright ©2021 All rights reserved |
+            | Copyright ©2020-2025 All rights reserved |
             <a href="mailto:devilliers.matthieu@gmail.com" target="_blank" class="link-light">Contactez-moi</a>
         </span>
     </div>
